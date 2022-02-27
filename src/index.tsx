5b360e0f10924b0eb422aab9f2e4f3649e25086e
Raquel Moreira <rakelmoreiramendonca@gmail.com>
2022-02-27 16:07:27 -0300

    Listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2022-02-27 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date("2021-08-31 17:00:00"),
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
