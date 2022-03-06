de90cca20c247a67f6d7a17aba0e9fc012553ab6
Raquel Moreira <rakelmoreiramendonca@gmail.com>
2022-03-06 12:14:41 -0300

    Criando hook

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -21,7 +21,7 @@ interface TransactionsContextData {
     createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
 );
 
